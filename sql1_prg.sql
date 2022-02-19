81e474d1a1deaad49f7bfeaa963122c5569a92ce
Shireesha <99179617+Shireesha-perspective-data@users.noreply.github.com>
2022-02-20 01:17:07 +0530

    Add files via upload
    Here explain about the DataSet. how to create a database and  table in database and also how to  insert the values in database

---
+++ sql1_prg.sql
@@ -0,0 +1,12 @@
+/*---here create a DataBase_name--
+ here a create a table
+ here we are insert a columns values. 
+*/
+
+/*create database Database_object;*/
+ create table Database_name.table2(col1 int,col2 varchar(255),col3 varchar(255)); 
+insert into Database_name.table2(col1,col2,col3)        
+values(col1,'s','car'),(2,'p','BMW'),(3,'c','TATA'),(4,'s','bike');  
+
+
+ 
---
+++ student_result.sql
@@ -0,0 +1,3 @@
+insert into student_result.table1(col1,col2,col3)
+values(1,'Rehman','pass'),(2,'Reethu','pass'),(3,'Manoj','pass');
+select * from student_result.table1 limit 3;
