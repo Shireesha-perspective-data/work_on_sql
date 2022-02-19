37c0ca6097c224a4745d20827816c2d887beaac7
Shireesha <99179617+Shireesha-perspective-data@users.noreply.github.com>
2022-02-20 01:30:53 +0530

    Add files via upload
    students names with there result to be executed.

---
+++ students_names_with_result.sql
@@ -0,0 +1,4 @@
+insert into student_result.table1(col1,col2,col3)
+values(1,'Rehman','pass'),(2,'Reethu','pass'),(3,'Manoj','pass');
+select * from student_result.table1 limit 3;
+select col3,col2 from student_result.table1 limit 3; 
