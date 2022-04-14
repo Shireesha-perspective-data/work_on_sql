964cf5ad8d5f703f86bd6dac314251e599f8a660
Shireesha <99179617+Shireesha-perspective-data@users.noreply.github.com>
2022-04-15 00:19:54 +0530

    Update sql_prg.sql

--- sql_prg.sql
+++ sql_prg.sql
@@ -7,6 +7,3 @@
  create table Database_name.table2(col1 int,col2 varchar(255),col3 varchar(255)); 
 insert into Database_name.table2(col1,col2,col3)        
 values(col1,'s','car'),(2,'p','BMW'),(3,'c','TATA'),(4,'s','bike');  
-
-
- 
