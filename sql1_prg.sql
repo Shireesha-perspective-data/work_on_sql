d87affac19d54aa5f431b90560907dc0ad671576
Shireesha <99179617+Shireesha-perspective-data@users.noreply.github.com>
2022-04-15 00:10:11 +0530

    Update sql1_prg.sql

--- sql1_prg.sql
+++ sql1_prg.sql
@@ -5,8 +5,5 @@
 
 /*create database Database_object;*/
  create table Database_name.table2(col1 int,col2 varchar(255),col3 varchar(255)); 
-insert into Database_name.table2(col1,col2,col3)        
-values(col1,'s','car'),(2,'p','BMW'),(3,'c','TATA'),(4,'s','bike');  
-
-
- 
+insert into Database_name.table2(col1,col2,col3)    
+values(col1,'s','car'),(2,'p','BMW'),(3,'c','TATA'),(4,'s','bike');  /* insert the values in the table name is "table2"*/    
