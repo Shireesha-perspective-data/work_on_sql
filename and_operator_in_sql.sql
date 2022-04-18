db315299694c9035924e8e49f9f0733537e2e5a7
Shireesha <99179617+Shireesha-perspective-data@users.noreply.github.com>
2022-04-18 22:26:57 +0530

    Update and_operator_in_sql.sql

--- and_operator_in_sql.sql
+++ and_operator_in_sql.sql
@@ -1,4 +1,3 @@
 /*The AND operator displays a record if all the conditions separated by AND are TRUE.*/
 select * from sakila.actor 
-where actor_id>10 and first_name like "SUSAN%";
- 
+where actor_id>10 and first_name like "SUSAN%"; 
