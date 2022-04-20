4f34136dd7963f74d2ec4eb71f6933dd46c93ec9
Shireesha <99179617+Shireesha-perspective-data@users.noreply.github.com>
2022-04-20 23:27:47 +0530

    Update students_details.sql

--- students_details.sql
+++ students_details.sql
@@ -1,4 +1,5 @@
 /*create table student_result.table2(col1 int,col2 varchar(255),col3 varchar(255),col4 varchar(255),col5 int,col6 int)*/
+
 insert into student_result.table2(col1 , col2,col3,col4 ,col5,col6)
 values(1,'Rehman','ranju','venkey',123,34223),(2,'raj','ramk','shiv',87,456),(3,'Malika','hjj','Har',76,1098),(4,'cinema','hjik','ram',98,987); /*insert the values in the given ttable*/
 select * from student_result.table2;
