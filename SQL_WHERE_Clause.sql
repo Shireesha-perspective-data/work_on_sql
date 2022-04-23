b1fd4aa0d3c68d5338aab06159258ab69ae200f1
Shireesha <99179617+Shireesha-perspective-data@users.noreply.github.com>
2022-04-23 23:29:41 +0530

    Update SQL_WHERE_Clause.sql

--- SQL_WHERE_Clause.sql
+++ SQL_WHERE_Clause.sql
@@ -1,4 +1,4 @@
-/*The WHERE clause is used to filter records.
-It is used to extract only those records that fulfill a specified condition.and also the WHERE Clause not only used Statements,It also used to UPDATE,DELETE,.etc  */
+/*The WHERE clause is used to filter records.*/
+/*It is used to extract only those records that fulfill a specified condition.and also the WHERE Clause not only used Statements,It also used to UPDATE,DELETE,.etc  */
 select * from sarila.actor
 where actor_id>25; 
